b2f7564743f58860e4ba16fe96dd0de489f3ef3b
aashishrawte <aashishrawte@digital-nirvana.in>
2022-05-12 10:12:44 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+// variable
+
+// string, number, object - datatype
+
+// let const var
+
+// a = 0;
+
+// b = 'piyush';
+
+// arr = [5, 4, 6, 9, 2, 10];
+
+// console.log(arr.sort());
+
+// *
+// **
+// ***
+// ****
+// *****
+
+// let n = 5;
+// let str = '';
+
+// for(let i = 0; i < n; i++){
+//     for(let j=0; j <= i; j++){{
+        
+//         str+='*';
+
+//     }}
+//     str+='\n';
+// }
+// console.log(str);
+
+
+// function piyush(p1, p2) {
+//     return p1 + p2;
+// }
+
+// const result = piyush(2,3);
+
+// console.log(result);
+
+// arrow function
+
+// const result = (p1,p2) => {
+//     console.log(p1+p2);
+// }
+
+// result(5,6);
+
+// ()=>{}
+
